e7dff865378253633744f6145872e525acda44e2
RPL-20 <nurulhakiki689@gmail.com>
2022-06-16 14:37:34 +0700

    ini merupakan kode program tentang jasa online

--- app/src/main/java/nurulhakiki/polbeng/ac/id/jasaonline/services/UserService.kt
+++ app/src/main/java/nurulhakiki/polbeng/ac/id/jasaonline/services/UserService.kt
@@ -1,7 +1,13 @@
 package nurulhakiki.polbeng.ac.id.jasaonline.services
 
+import nurulhakiki.polbeng.ac.id.jasaonline.models.LoginResponse
+import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.QueryMap
 
 interface UserService {
-    @GET()
+    @GET("login")
+    fun loginUser(
+        @QueryMap filter:HashMap<String, String>
+    ): Call<LoginResponse>
 }
