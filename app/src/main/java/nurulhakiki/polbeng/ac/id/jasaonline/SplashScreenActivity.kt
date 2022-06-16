8e23efb24c521b1b5470b8360ebbee33adc6f6b1
RPL-20 <nurulhakiki689@gmail.com>
2022-06-16 09:46:41 +0700

    ini merupakan kode program tentang jasa online

--- app/src/main/java/nurulhakiki/polbeng/ac/id/jasaonline/SplashScreenActivity.kt
+++ app/src/main/java/nurulhakiki/polbeng/ac/id/jasaonline/SplashScreenActivity.kt
@@ -7,6 +7,7 @@ import android.os.Handler
 import nurulhakiki.polbeng.ac.id.jasaonline.helpers.Config
 
 class SplashScreenActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
