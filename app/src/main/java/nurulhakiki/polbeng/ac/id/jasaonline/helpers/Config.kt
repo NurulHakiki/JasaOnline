d015ba3f16da53ae4063c341062c769d06828f27
RPL-20 <nurulhakiki689@gmail.com>
2022-06-16 14:57:23 +0700

    ini merupakan kode program tentang jasa online

--- app/src/main/java/nurulhakiki/polbeng/ac/id/jasaonline/helpers/Config.kt
+++ app/src/main/java/nurulhakiki/polbeng/ac/id/jasaonline/helpers/Config.kt
@@ -3,5 +3,7 @@ package nurulhakiki.polbeng.ac.id.jasaonline.helpers
 class Config {
     companion object {
         const val SPLASH_TIME_OUT:Long = 3000
+        const val BASE_URL = "http://192.168.57.2:3000/"
+        const val EXTRA_FRAGMENT_ID = "FRAGMENT_ID"
     }
 }
---
+++ app/src/main/java/nurulhakiki/polbeng/ac/id/jasaonline/services/ServiceBuilder.kt
@@ -0,0 +1,21 @@
+package nurulhakiki.polbeng.ac.id.jasaonline.services
+
+import nurulhakiki.polbeng.ac.id.jasaonline.helpers.Config
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ServiceBuilder {
+    //create okhttp client
+    private val okHttp: OkHttpClient.Builder = OkHttpClient.Builder()
+    //create retrofit builder
+    private val builder: Retrofit.Builder =
+        Retrofit.Builder().baseUrl(Config.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttp.build())
+    //create retrofit instance
+    private val retrofit: Retrofit = builder.build()
+    fun <T> buildService(serviceType: Class<T>): T {
+        return retrofit.create(serviceType)
+    }
+}
